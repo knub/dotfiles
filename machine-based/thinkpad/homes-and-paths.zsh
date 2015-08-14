0f2a93f486a01bcbe4d7cdcb35054dbc4df8023f
Stefan Bunk <stefan.bunk@googlemail.com>
2015-08-14 20:37:43 +0200

    Split into machine based and independent configuration.

---
+++ machine-based/thinkpad/homes-and-paths.zsh
@@ -0,0 +1,22 @@
+## PATH
+export PATH=$PATH:/opt/scala-2.11.5/bin
+export PATH="$DOTFILES/bin:$PATH"
+export PATH="$PATH:/opt/idea-IU-139.659.2/bin"
+
+## JAVA HOME
+#export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_31
+
+## HADOOP_HOME
+export HADOOP_HOME="/home/knub/Programme/hadoop-2.5.1"
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+
+## ANDROID
+export ANDROID_HOME="/opt/android-sdk"
+
+## CAFFE
+export CAFFE_ROOT="/opt/caffe-tmbo"
+export PYTHONPATH="$CAFFE_ROOT/python"
+
+## QtCreator
+alias qtcreator="/opt/Qt5.4.1/Tools/QtCreator/bin/qtcreator.sh"
