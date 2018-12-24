9594e0338e6a20ea9dbdf4aab048865bf594eb90
Stefan Bunk <stefan@merantix.com>
2018-12-24 01:23:05 +0100

    Move to ubuntu again.

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGE_MANAGER=dnf
+PACKAGE_MANAGER=apt
 
 set -o xtrace
 
@@ -9,12 +9,9 @@ if ! test -f zsh/antigen/Makefile ; then
 	git submodule update
 fi
 
-if ! grep -q "defaultyes" /etc/dnf/dnf.conf ; then
-	echo "defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf
-fi
-
 sudo $PACKAGE_MANAGER upgrade
 sudo $PACKAGE_MANAGER install \
+	acpi-call-dkms \
 	alacarte \
 	autokey-gtk \
 	curl \
@@ -25,18 +22,18 @@ sudo $PACKAGE_MANAGER install \
 	gitg \
 	gnome-tweak-tool \
 	gparted \
-	gvim \
+	vim-gtk3 \
 	iotop \
-	ImageMagick \
+	imagemagick \
 	inkscape \
 	meld \
 	nethogs \
 	ncdu \
-	rubygem-rake \
+	rake \
+	smartmontools \
 	sl \
-	the_silver_searcher \
+	silversearcher-ag \
 	tree \
-	texlive \
 	tilda \
 	tlp \
 	tlp-rdw \
@@ -50,19 +47,15 @@ if ! grep -q "knub:/bin/zsh" /etc/passwd ; then
 	chsh -s /bin/zsh
 fi
 
-if ! dnf repolist | grep -q "home_snwh_paper" ; then
-	sudo dnf config-manager --add-repo http://download.opensuse.org/repositories/home:snwh:paper/Fedora_25/home:snwh:paper.repo
-	sudo dnf install paper-gtk-theme paper-icon-theme
-fi
 
 if ! test -L ~/.zshrc ; then
 	rake install
 fi
 
-###
-### Post installation steps
-###
-# * Install Hack font: http://sourcefoundry.org/hack/
+
+# Vim: run `:BundleInstall`
+# open `powertop`
+
 
 ###
 ### Gnome Shell Extensions
