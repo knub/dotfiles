63aefa97f612ed7f0359598c8024fd196009d091
Stefan Bunk <stefan.bunk@vara.ai>
2025-08-10 22:25:48 +0200

    Mac adaptions.

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-PACKAGE_MANAGER=apt
-
 set -o xtrace
 
 if ! test -f zsh/antigen/Makefile ; then
@@ -9,54 +7,18 @@ if ! test -f zsh/antigen/Makefile ; then
 	git submodule update
 fi
 
-sudo $PACKAGE_MANAGER upgrade
-sudo $PACKAGE_MANAGER install \
-	curl \
-	dos2unix \
+sudo brew install \
+	coreutils \
 	htop \
-	git \
-	gitg \
-	gnome-tweaks \
-	gnome-shell-extension-prefs \
-	gparted \
-	guake \
 	imagemagick \
-	inkscape \
-	meld \
 	moreutils \
 	ncdu \
-	python3-full \
-	python3-gpg \
-	python3-pip \
-	rake \
-	silversearcher-ag \
-	tree \
-	ubuntu-restricted-extras \
-	ubuntu-restricted-addons \
-	vim \
-	vim-gtk3 \
-	xclip \
-	zsh
+	the_silver_searcher
 
 # Backup ideas:
 # acpi-call-tools
 # tlp
 # tlp-rdw
-
-sudo snap install zoom-client
-sudo snap install spotify
-sudo snap install joplin
-sudo snap install joplin-desktop
-sudo snap install signal-desktop
-sudo snap install zotero-snap
-sudo snap install pycharm-professional --classic
-sudo snap install google-cloud-cli --classic
-
-
-if ! grep -q "knub:/bin/zsh" /etc/passwd ; then
-	chsh -s /bin/zsh
-fi
-
 
 if ! test -L ~/.zshrc ; then
 	rake install
